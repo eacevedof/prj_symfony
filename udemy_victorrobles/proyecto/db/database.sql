18a0c9341599ba07df6a88b72ce869ed46d92192
Eduardo A.F <eacevedof@yahoo.es>
2020-01-04 18:16:02 +0100

    sec101 458. Crear la base de datos 6 min corrijo script tasks

--- udemy_victorrobles/proyecto/db/database.sql
+++ udemy_victorrobles/proyecto/db/database.sql
@@ -13,14 +13,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-CREATE TABLE IF NOT EXISTS tasks(
-    id          int(11) not null auto_incrment,
-    user_id     int(11) not null,
-    title       varchar(100),
-    content     text,
-    priority    varchar(20),
-    hours       int(8),
-    created_at  datetime,
-    CONSTRAINT pk_task PRIMARY KEY(id),
-    CONSTRAINT fk_task_user FOREIGN KEY(user_id) REFERENCES users(id)
-) ENGINE=InnoDb;
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `priority` varchar(20) DEFAULT NULL,
+  `hours` int(8) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
