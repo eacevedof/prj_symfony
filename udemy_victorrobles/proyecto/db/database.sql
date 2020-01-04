f19bea7b4049de13dc65479eb929f180c3c5b3a0
Eduardo A.F <eacevedof@yahoo.es>
2020-01-04 18:34:34 +0100

    database.sql de dump

--- udemy_victorrobles/proyecto/db/database.sql
+++ udemy_victorrobles/proyecto/db/database.sql
@@ -1,8 +1,45 @@
-CREATE DATABASE IF NOT EXISTS symf_tasks;
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.5.5-10.4.11-MariaDB-1:10.4.11+maria~bionic : Database - symf_tasks
+*********************************************************************
+*/
 
-USE symf_tasks;
 
-CREATE TABLE IF NOT EXISTS `users` (
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`symf_tasks` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `symf_tasks`;
+
+/*Table structure for table `tasks` */
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `priority` varchar(20) DEFAULT NULL,
+  `hours` int(8) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  CONSTRAINT pk_task PRIMARY KEY(id),
+  CONSTRAINT fk_task_user FOREIGN KEY(user_id) REFERENCES users(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tasks` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(50) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
@@ -11,15 +48,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL,
-  `content` text DEFAULT NULL,
-  `priority` varchar(20) DEFAULT NULL,
-  `hours` int(8) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
