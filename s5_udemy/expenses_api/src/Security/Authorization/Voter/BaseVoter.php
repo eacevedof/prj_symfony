5aa482b813b86333b961eeaaca893c62e581b4b9
ioedumac <eacevedof@yahoo.es>
2020-04-04 12:50:10 +0200

    src/Security/Authorization/Voter/BaseVoter.php  copio de repo original

--- s5_udemy/expenses_api/src/Security/Authorization/Voter/BaseVoter.php
+++ s5_udemy/expenses_api/src/Security/Authorization/Voter/BaseVoter.php
@@ -1,24 +1,19 @@
 <?php
 //src/Security/Authorization/Voter/BaseVoter.php
 declare(strict_types=1);
+
 namespace App\Security\Authorization\Voter;
 
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Security;
 
-/*
- * La clase abstracta nos va a permitir que los hijos hereden de BaseVoter y que
- * puedan customizar los metodos obligatorios y la interfaz no obliga a la clase abstracta
- * a implementar los metodos de la interfaz
- * */
-abstract class BaseVoter extends Voter //Voter implementa: VoterInterface
+abstract class BaseVoter extends Voter
 {
-
     protected Security $security;
 
     public function __construct(Security $security)
     {
         $this->security = $security;
     }
-
 }
